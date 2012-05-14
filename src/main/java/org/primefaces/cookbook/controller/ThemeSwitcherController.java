47ddf54e9ffe6333669912d25a883ace5dddde7a
Oleg Varaksin <ovaraksin@googlemail.com>
2012-05-14 22:38:39 +0200

    Checked in init project structure

---
+++ src/main/java/org/primefaces/cookbook/controller/ThemeSwitcherController.java
@@ -0,0 +1,10 @@
+package org.primefaces.cookbook.controller;
+
+/**
+ * ThemeSwitcherController
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+public class ThemeSwitcherController {
+}
---
+++ src/main/java/org/primefaces/cookbook/model/AvailableThemes.java
@@ -0,0 +1,10 @@
+package org.primefaces.cookbook.model;
+
+/**
+ * AvailableThemes
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+public class AvailableThemes {
+}
