cc41239d088ac4cdf33e7f98278ca6098f128e04
Oleg Varaksin <ovaraksin@googlemail.com>
2012-05-20 14:14:06 +0200

    Started working on chapter 2

--- src/main/java/org/primefaces/cookbook/controller/BookController.java
+++ src/main/java/org/primefaces/cookbook/controller/BookController.java
@@ -32,25 +32,52 @@ public class BookController implements Serializable {
 	public BookController() {
 		root = new DefaultTreeNode("Root", null);
 
-		TreeNode chapter1 = new DefaultTreeNode(new BookTreeNode("Chapter 1", null), root);
-		TreeNode chapter2 = new DefaultTreeNode(new BookTreeNode("Chapter 2", null), root);
-		TreeNode chapter3 = new DefaultTreeNode(new BookTreeNode("Chapter 3", null), root);
-		TreeNode chapter4 = new DefaultTreeNode(new BookTreeNode("Chapter 4", null), root);
-		TreeNode chapter5 = new DefaultTreeNode(new BookTreeNode("Chapter 5", null), root);
-		TreeNode chapter6 = new DefaultTreeNode(new BookTreeNode("Chapter 6", null), root);
-		TreeNode chapter7 = new DefaultTreeNode(new BookTreeNode("Chapter 7", null), root);
-		TreeNode chapter8 = new DefaultTreeNode(new BookTreeNode("Chapter 8", null), root);
-		TreeNode chapter9 = new DefaultTreeNode(new BookTreeNode("Chapter 9", null), root);
-		TreeNode chapter10 = new DefaultTreeNode(new BookTreeNode("Chapter 10", null), root);
-		TreeNode chapter11 = new DefaultTreeNode(new BookTreeNode("Chapter 11", null), root);
-		TreeNode chapter12 = new DefaultTreeNode(new BookTreeNode("Chapter 12", null), root);
+		TreeNode chapter1 = new DefaultTreeNode(new BookTreeNode("Getting Started with PrimeFaces", null), root);
+		TreeNode chapter2 = new DefaultTreeNode(new BookTreeNode("Theming Concept", null), root);
+		TreeNode chapter3 = new DefaultTreeNode(new BookTreeNode("Enhanced Inputs and Selects", null), root);
+		TreeNode chapter4 = new DefaultTreeNode(new BookTreeNode("Grouping Content with Panels", null), root);
+		TreeNode chapter5 = new DefaultTreeNode(new BookTreeNode("Data Iteration Components", null), root);
+		TreeNode chapter6 = new DefaultTreeNode(new BookTreeNode("Endless Menu Variations", null), root);
+		TreeNode chapter7 = new DefaultTreeNode(new BookTreeNode("Working with Files and Images", null), root);
+		TreeNode chapter8 = new DefaultTreeNode(new BookTreeNode("Drag Me, Drop Me", null), root);
+		TreeNode chapter9 = new DefaultTreeNode(new BookTreeNode("Creating Charts and Maps", null), root);
+		TreeNode chapter10 = new DefaultTreeNode(new BookTreeNode("Miscellaneous, Advanced Use Cases", null), root);
+		TreeNode chapter11 = new DefaultTreeNode(new BookTreeNode("Writing of Custom Components", null), root);
+		TreeNode chapter12 = new DefaultTreeNode(new BookTreeNode("PrimeFaces Extensions in Action", null), root);
+
+		// chapter 1
 
 		// chapter 2
+		new DefaultTreeNode(NODE_TYPE, new BookTreeNode("Customizing theme styles", "chapter2/customThemeStyles.xhtml"),
+		                    chapter2);
+		new DefaultTreeNode(NODE_TYPE,
+		                    new BookTreeNode("Disabling default styles on input components",
+		                                     "chapter2/disableDefaultStyles.xhtml"), chapter2);
 		new DefaultTreeNode(NODE_TYPE,
 		                    new BookTreeNode("Standard theme switcher of PrimeFaces", "chapter2/stdThemeSwitcher.xhtml"),
 		                    chapter2);
 		new DefaultTreeNode(NODE_TYPE, new BookTreeNode("Alternative stateful theme switcher", "chapter2/altThemeSwitcher.xhtml"),
 		                    chapter2);
+
+		// chapter 3
+
+		// chapter 4
+
+		// chapter 5
+
+		// chapter 6
+
+		// chapter 7
+
+		// chapter 8
+
+		// chapter 9
+
+		// chapter 10
+
+		// chapter 11
+
+		// chapter 12
 	}
 
 	public TreeNode getRoot() {
