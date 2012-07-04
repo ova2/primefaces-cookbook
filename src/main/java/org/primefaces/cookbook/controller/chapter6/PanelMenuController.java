806bf4ad6f1b0bf0c7ba7842522c050efa749557
Oleg Varaksin <ovaraksin@googlemail.com>
2012-07-04 22:05:58 +0200

    Implemented PanelMenu

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter6/PanelMenuController.java
@@ -0,0 +1,15 @@
+package org.primefaces.cookbook.controller.chapter6;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * PanelMenuController
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class PanelMenuController extends BaseMenuController {
+}
