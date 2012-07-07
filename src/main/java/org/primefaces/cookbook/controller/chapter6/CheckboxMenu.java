3378d0079fc63814ea70ff997539228bd042b7db
Oleg Varaksin <ovaraksin@googlemail.com>
2012-07-07 10:39:03 +0200

    Implemented CheckboxMenu

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter6/CheckboxMenu.java
@@ -0,0 +1,71 @@
+package org.primefaces.cookbook.controller.chapter6;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+
+/**
+ * CheckboxMenu
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class CheckboxMenu implements Serializable {
+
+	private List<SelectItem> languages;
+
+	private Map<String, String> color;
+
+	private List<Locale> selectedLanguages;
+
+	private List<String> selectedColors;
+
+	public List<SelectItem> getLanguages() {
+		if (languages == null) {
+			languages = new ArrayList<SelectItem>();
+			languages.add(new SelectItem(new Locale("de"), "German"));
+			languages.add(new SelectItem(new Locale("en"), "English"));
+			languages.add(new SelectItem(new Locale("it"), "Italian"));
+			languages.add(new SelectItem(new Locale("fr"), "French"));
+		}
+
+		return languages;
+	}
+
+	public Map<String, String> getColors() {
+		if (color == null) {
+			color = new HashMap<String, String>();
+			color.put("Red", "Red");
+			color.put("Green", "Green");
+			color.put("Blue", "Blue");
+			color.put("Orange", "Orange");
+		}
+
+		return color;
+	}
+
+	public List<Locale> getSelectedLanguages() {
+		return selectedLanguages;
+	}
+
+	public void setSelectedLanguages(List<Locale> selectedLanguages) {
+		this.selectedLanguages = selectedLanguages;
+	}
+
+	public List<String> getSelectedColors() {
+		return selectedColors;
+	}
+
+	public void setSelectedColors(List<String> selectedColors) {
+		this.selectedColors = selectedColors;
+	}
+}
---
+++ src/main/java/org/primefaces/cookbook/converter/LocaleConverter.java
@@ -0,0 +1,86 @@
+package org.primefaces.cookbook.converter;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * LocaleConverter
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@FacesConverter(value = "org.primefaces.cookbook.converter.LocaleConverter")
+public class LocaleConverter implements Converter, Serializable {
+
+	public Object getAsObject(final FacesContext fc, final UIComponent component, final String value) {
+		if (StringUtils.isBlank(value)) {
+			return fc.getApplication().getDefaultLocale();
+		}
+
+		return getLocaleObject(value);
+	}
+
+	public String getAsString(final FacesContext fc, final UIComponent component, final Object value) {
+		if (value == null) {
+			final Locale defaultLocale = fc.getApplication().getDefaultLocale();
+			if (defaultLocale == null) {
+				return null;
+			}
+
+			return getLocaleString(defaultLocale);
+		}
+
+		if (value instanceof String) {
+			return (String) value;
+		} else if (value instanceof Locale) {
+			return getLocaleString((Locale) value);
+		} else {
+			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR,
+			                                              "Wrong type: '" + value.getClass().getSimpleName()
+			                                              + "' is not 'Locale'.", StringUtils.EMPTY));
+		}
+	}
+
+	public static Locale getLocaleObject(final String locale) {
+		final String[] parts = locale.split("_");
+		if (parts.length == 0
+		    || !parts[0].matches("[a-zA-Z]{2,2}")
+		    || (parts.length > 1 && parts[1].length() != 0 && !parts[1].matches("[a-zA-Z]{2,2}"))) {
+			throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR,
+			                                              "'" + locale + "' does not represent a valid locale",
+			                                              StringUtils.EMPTY));
+		}
+
+		switch (parts.length) {
+		case 3:
+			return new Locale(parts[0], parts[1], parts[2]);
+
+		case 2:
+			return new Locale(parts[0], parts[1]);
+
+		case 1:
+			return new Locale(parts[0]);
+
+		default:
+			return new Locale(parts[0], parts[1], StringUtils.join(ArrayUtils.subarray(parts, 2, parts.length), '_'));
+		}
+	}
+
+	public static String getLocaleString(final Locale locale) {
+		if (StringUtils.isBlank(locale.getCountry())) {
+			return locale.getLanguage();
+		}
+
+		return locale.getLanguage() + "_" + locale.getCountry();
+	}
+}
