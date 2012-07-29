5ccb06138379d778251a1f9fc4c6d055012a6302
Oleg Varaksin <ovaraksin@googlemail.com>
2012-07-29 12:32:32 +0200

    Finished drag & drop integration and the chapter 8

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter8/IntegrationDragDrop.java
@@ -0,0 +1,68 @@
+package org.primefaces.cookbook.controller.chapter8;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.cookbook.model.chapter8.Document;
+import org.primefaces.event.DragDropEvent;
+
+/**
+ * IntegrationDragDrop
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class IntegrationDragDrop implements Serializable {
+
+	private List<Document> availableDocs = new ArrayList<Document>();
+	private List<Document> deletedDocs = new ArrayList<Document>();
+
+	@PostConstruct
+	public void initialize() {
+		availableDocs.add(new Document("Perl script", 120, "Sara Schmidt", getCreationDate(), "perl"));
+		availableDocs.add(new Document("Alex's master thesis", 850, "Alex Konradi", getCreationDate(), "dvi"));
+		availableDocs.add(new Document("jQuery presentation", 687, "Jasper Morgan", getCreationDate(), "ppt"));
+		availableDocs.add(new Document("Russian songs", 1020, "Urmel Night", getCreationDate(), "mid"));
+		availableDocs.add(new Document("German songs", 2268, "Nicole Konradi", getCreationDate(), "mp3"));
+		availableDocs.add(new Document("Cool flash multimedia", 2268, "John Wiese", getCreationDate(), "swf"));
+		availableDocs.add(new Document("Windows custom sound", 84, "Sebastian Schwarz", getCreationDate(), "wav"));
+		availableDocs.add(new Document("Currency exchange table", 1400, "Nadja Swift", getCreationDate(), "xls"));
+		availableDocs.add(new Document("Search masks configuration", 33, "Andrew Andreev", getCreationDate(), "xml"));
+	}
+
+	public List<Document> getAvailableDocs() {
+		return availableDocs;
+	}
+
+	public List<Document> getDeletedDocs() {
+		return deletedDocs;
+	}
+
+	public void onDocumentDrop(DragDropEvent ddEvent) {
+		Document doc = (Document) ddEvent.getData();
+
+		deletedDocs.add(doc);
+		availableDocs.remove(doc);
+	}
+
+	private Date getCreationDate() {
+		Random random = new Random();
+		int day = random.nextInt(30);
+		int month = random.nextInt(Calendar.DECEMBER + 1);
+		int year = random.nextInt(2012);
+		GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+
+		return calendar.getTime();
+	}
+}
---
+++ src/main/java/org/primefaces/cookbook/model/chapter8/Document.java
@@ -0,0 +1,47 @@
+package org.primefaces.cookbook.model.chapter8;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Document
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+public class Document implements Serializable {
+
+	private String title;
+	private int size;
+	private String creator;
+	private Date creationDate;
+	private String extension;
+
+	public Document(String title, int size, String creator, Date creationDate, String extension) {
+		this.title = title;
+		this.size = size;
+		this.creator = creator;
+		this.creationDate = creationDate;
+		this.extension = extension;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+}
