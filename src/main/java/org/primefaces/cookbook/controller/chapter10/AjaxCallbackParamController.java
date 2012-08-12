f8fa4c1e02d9ad2db9b044d7d317716a1b213546
Oleg Varaksin <ovaraksin@googlemail.com>
2012-08-12 21:25:57 +0200

    Added Ajax Callback Parameters sample

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter10/AjaxCallbackParamController.java
@@ -0,0 +1,54 @@
+package org.primefaces.cookbook.controller.chapter10;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.context.RequestContext;
+
+/**
+ * AjaxCallbackParamController
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class AjaxCallbackParamController implements Serializable {
+
+	private String name;
+
+	public void save(ActionEvent ae) {
+		RequestContext requestContext = RequestContext.getCurrentInstance();
+
+		String message;
+		FacesMessage.Severity severity;
+
+		if ("PrimeFaces Cookbook".equals(name)) {
+			message = "All right!";
+			severity = FacesMessage.SEVERITY_INFO;
+
+			requestContext.addCallbackParam("validName", true);
+		} else {
+			message = "Name is wrong, try again";
+			severity = FacesMessage.SEVERITY_ERROR;
+
+			requestContext.addCallbackParam("validName", false);
+		}
+
+		FacesMessage msg = new FacesMessage(severity, message, null);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
