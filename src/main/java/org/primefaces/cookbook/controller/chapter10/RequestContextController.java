0fd5909874d9633b64aedf1909bf28b679b0bfb6
Oleg Varaksin <ovaraksin@googlemail.com>
2012-08-12 13:05:36 +0200

    Finished PFS and RequestContext (chapter 10)

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter10/RequestContextController.java
@@ -0,0 +1,55 @@
+package org.primefaces.cookbook.controller.chapter10;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.context.RequestContext;
+
+/**
+ * RequestContextController
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class RequestContextController implements Serializable {
+
+	private boolean firstOutput = true;
+
+	private int counter = 0;
+
+	public void incrementWithUpdate(ActionEvent ae) {
+		counter++;
+
+		RequestContext requestContext = RequestContext.getCurrentInstance();
+
+		if (firstOutput) {
+			requestContext.update("firstOutput");
+		} else {
+			requestContext.update("secondOutput");
+		}
+	}
+
+	public void incrementWithScroll(ActionEvent ae) {
+		counter++;
+
+		RequestContext requestContext = RequestContext.getCurrentInstance();
+		requestContext.scrollTo("counter");
+	}
+
+	public boolean isFirstOutput() {
+		return firstOutput;
+	}
+
+	public void setFirstOutput(boolean firstOutput) {
+		this.firstOutput = firstOutput;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+}
---
+++ src/main/java/org/primefaces/cookbook/validator/RequiredCheckboxValidator.java
@@ -0,0 +1,45 @@
+package org.primefaces.cookbook.validator;
+
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UISelectBoolean;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * RequiredCheckboxValidator
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@FacesValidator("org.primefaces.cookbook.validator.RequiredCheckboxValidator")
+public class RequiredCheckboxValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		if (!(component instanceof UISelectBoolean)) {
+			throw new IllegalArgumentException("RequiredCheckboxValidator must be registered on a UISelectBoolean");
+		}
+
+		if (!Boolean.TRUE.equals(value)) {
+			String requiredMessage = ((UIInput) component).getRequiredMessage();
+
+			if (requiredMessage == null && context.getApplication().getMessageBundle() != null) {
+				requiredMessage =
+				    ResourceBundle.getBundle(context.getApplication().getMessageBundle(), context.getViewRoot().getLocale())
+				                  .getString(UIInput.REQUIRED_MESSAGE_ID);
+			}
+
+			if (requiredMessage == null) {
+				requiredMessage = component.getAttributes().get("label") + ": Validation Error: Value is required.";
+			}
+
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, requiredMessage, null));
+		}
+	}
+}
