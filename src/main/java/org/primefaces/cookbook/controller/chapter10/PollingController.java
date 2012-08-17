dc45da9ee666da8028f43cc1ef9655a8f0fa9814
Oleg Varaksin <ovaraksin@googlemail.com>
2012-08-17 06:48:09 +0200

    Added polling sample

---
+++ src/main/java/org/primefaces/cookbook/controller/chapter10/PollingController.java
@@ -0,0 +1,30 @@
+package org.primefaces.cookbook.controller.chapter10;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ * PollingController
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean
+@ViewScoped
+public class PollingController implements Serializable {
+
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
+
+	public void showMessage() {
+		FacesMessage msg =
+		    new FacesMessage(FacesMessage.SEVERITY_INFO, "Updated feed at " + simpleDateFormat.format(new Date()) + " o'clock",
+		                     null);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+}
