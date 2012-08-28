21d5e0ea5a6a47de55d362b872614b102b51a952
Mert <mert@t2.com.tr>
2012-08-28 21:42:51 +0300

    fix on chapter4 code

--- src/main/java/org/primefaces/cookbook/controller/chapter4/ScrollPanelController.java
+++ src/main/java/org/primefaces/cookbook/controller/chapter4/ScrollPanelController.java
@@ -1,5 +1,6 @@
 package org.primefaces.cookbook.controller.chapter4;
 
+import org.primefaces.cookbook.converter.CarConverter;
 import org.primefaces.cookbook.model.chapter3.Car;
 
 import javax.faces.bean.ManagedBean;
@@ -16,18 +17,7 @@ import java.util.List;
 @ViewScoped
 public class ScrollPanelController implements Serializable {
 
-    List<Car> cars = new ArrayList<Car>();
-    {
-        cars.add(new Car("CC"));
-        cars.add(new Car("Golf"));
-        cars.add(new Car("Jetta"));
-        cars.add(new Car("Passat"));
-        cars.add(new Car("Polo"));
-        cars.add(new Car("Scirocco"));
-        cars.add(new Car("Touareg"));
-    }
-
     public List<Car> getCars() {
-        return cars;
+        return new ArrayList<Car>(CarConverter.cars.values());
     }
 }
