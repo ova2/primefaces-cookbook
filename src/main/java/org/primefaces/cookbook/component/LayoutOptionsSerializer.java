a2b49573e2a791961eb6416c2fba26eae6393885
Oleg Varaksin <ovaraksin@googlemail.com>
2012-10-01 20:27:43 +0200

    Implemented custom Layout component (chapter 11)

---
+++ src/main/java/org/primefaces/cookbook/component/LayoutOptionsSerializer.java
@@ -0,0 +1,70 @@
+package org.primefaces.cookbook.component;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * LayoutOptionsSerializer
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+public class LayoutOptionsSerializer implements JsonSerializer<LayoutOptions> {
+
+	public JsonElement serialize(LayoutOptions src, Type typeOfSrc, JsonSerializationContext context) {
+		JsonObject result = new JsonObject();
+
+		Set<Map.Entry<String, Object>> options = src.getOptions().entrySet();
+		for (Map.Entry<String, Object> entry : options) {
+			Object value = entry.getValue();
+			JsonPrimitive jsonPrimitive = null;
+
+			if (value instanceof Boolean) {
+				jsonPrimitive = new JsonPrimitive((Boolean) value);
+			} else if (value instanceof Number) {
+				jsonPrimitive = new JsonPrimitive((Number) value);
+			} else if (value instanceof String) {
+				jsonPrimitive = new JsonPrimitive((String) entry.getValue());
+			}
+
+			result.add(entry.getKey(), jsonPrimitive);
+		}
+
+		if (src.getPanesOptions() != null) {
+			result.add("panes", context.serialize(src.getPanesOptions(), src.getPanesOptions().getClass()));
+		}
+
+		if (src.getNorthOptions() != null) {
+			result.add("north", context.serialize(src.getNorthOptions(), src.getNorthOptions().getClass()));
+		}
+
+		if (src.getSouthOptions() != null) {
+			result.add("south", context.serialize(src.getSouthOptions(), src.getSouthOptions().getClass()));
+		}
+
+		if (src.getWestOptions() != null) {
+			result.add("west", context.serialize(src.getWestOptions(), src.getWestOptions().getClass()));
+		}
+
+		if (src.getEastOptions() != null) {
+			result.add("east", context.serialize(src.getEastOptions(), src.getEastOptions().getClass()));
+		}
+
+		if (src.getCenterOptions() != null) {
+			result.add("center", context.serialize(src.getCenterOptions(), src.getCenterOptions().getClass()));
+		}
+
+		if (src.getChildOptions() != null) {
+			result.add("childOptions", context.serialize(src.getChildOptions(), src.getChildOptions().getClass()));
+		}
+
+		return result;
+	}
+}
---
+++ src/main/java/org/primefaces/cookbook/component/OpenEvent.java
@@ -0,0 +1,32 @@
+package org.primefaces.cookbook.component;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.behavior.Behavior;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.AjaxBehaviorListener;
+import javax.faces.event.FacesListener;
+
+/**
+ * OpenEvent
+ *
+ * @author  Oleg Varaksin / last modified by $Author: $
+ * @version $Revision: 1.0 $
+ */
+public class OpenEvent extends AjaxBehaviorEvent {
+
+	public OpenEvent(UIComponent component, Behavior behavior) {
+		super(component, behavior);
+	}
+
+	@Override
+	public boolean isAppropriateListener(FacesListener facesListener) {
+		return (facesListener instanceof AjaxBehaviorListener);
+	}
+
+	@Override
+	public void processListener(FacesListener facesListener) {
+		if (facesListener instanceof AjaxBehaviorListener) {
+			((AjaxBehaviorListener) facesListener).processAjaxBehavior(this);
+		}
+	}
+}
