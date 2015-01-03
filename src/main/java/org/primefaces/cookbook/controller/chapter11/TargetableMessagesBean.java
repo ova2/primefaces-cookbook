0b9c6c53c5cd6199680f2036a716dc0580c51d70
Oleg Varaksin <ovaraksin@googlemail.com>
2015-01-03 20:16:59 +0100

    Targetable messages

--- src/main/java/org/primefaces/cookbook/controller/chapter11/TargetableMessagesBean.java
+++ src/main/java/org/primefaces/cookbook/controller/chapter11/TargetableMessagesBean.java
@@ -7,42 +7,52 @@ import javax.inject.Named;
 import java.io.Serializable;
 
 /**
- * TargetableMessagesController
+ * TargetableMessagesBean
  *
- * @author  Oleg Varaksin / last modified by $Author: $
+ * @author Oleg Varaksin / last modified by $Author: $
  * @version $Revision: 1.0 $
  */
 @Named
 @ViewScoped
 public class TargetableMessagesBean implements Serializable {
 
-	public String addSaveMessage() {
-		addMessage("save", FacesMessage.SEVERITY_INFO, "Sample info message", "First data was successfully saved");
-		addMessage("save", FacesMessage.SEVERITY_INFO, "Sample info message", "Second data was successfully saved");
+    public String addSaveMessage() {
+        addMessage("save", FacesMessage.SEVERITY_INFO,
+                "Sample info message",
+                "First data was successfully saved");
+        addMessage("save", FacesMessage.SEVERITY_INFO,
+                "Sample info message",
+                "Second data was successfully saved");
 
-		return null;
-	}
+        return null;
+    }
 
-	public String addChangeMessage() {
-		addMessage("change", FacesMessage.SEVERITY_INFO, "Sample info message", "Data was successfully changed");
+    public String addChangeMessage() {
+        addMessage("change", FacesMessage.SEVERITY_INFO,
+                "Sample info message",
+                "Data was successfully changed");
 
-		return null;
-	}
+        return null;
+    }
 
-	public String addDeleteMessage() {
-		addMessage(null, FacesMessage.SEVERITY_INFO, "Sample info message", "Data was successfully deleted");
+    public String addDeleteMessage() {
+        addMessage(null, FacesMessage.SEVERITY_INFO,
+                "Sample info message",
+                "Data was successfully deleted");
 
-		return null;
-	}
+        return null;
+    }
 
-	public String addErrorMessage() {
-		addMessage(null, FacesMessage.SEVERITY_ERROR, "Sample error message", "Operation failed");
+    public String addErrorMessage() {
+        addMessage(null, FacesMessage.SEVERITY_ERROR,
+                "Sample error message",
+                "Operation failed");
 
-		return null;
-	}
+        return null;
+    }
 
-	private void addMessage(String key, FacesMessage.Severity severity, String message, String detail) {
-		FacesMessage msg = new FacesMessage(severity, message, detail);
-		FacesContext.getCurrentInstance().addMessage(key, msg);
-	}
+    private void addMessage(String key, FacesMessage.Severity severity, String message, String detail) {
+        FacesMessage msg = new FacesMessage(severity, message, detail);
+        FacesContext.getCurrentInstance().addMessage(key, msg);
+    }
 }
